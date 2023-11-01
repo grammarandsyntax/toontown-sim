d93b65bce974c6d2ea7827b53b27c8d092e051e4
grammarandsyntax <144465898+grammarandsyntax@users.noreply.github.com>
2023-11-01 10:21:37 -0400

    get name

--- Main/src/Cog.java
+++ Main/src/Cog.java
@@ -3,14 +3,22 @@ public class Cog {
     private String name;
     private int level;
     private int hp;
+    
 
     public Cog (String n, int l){
         name = n;
         level = l;
         hp = (level+1)*(level+2);
     }
+    public Cog (int l){
+      level = l;
+      hp = (level+1)*(level+2);
+    }
 
-    //methods
+    //get
+    public String getName(){
+      return name;
+    }
     public int getLevel(){
         return level;
     }
