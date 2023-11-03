7a136a643351f64906edb6636316356aea410587
grammarandsyntax <144465898+grammarandsyntax@users.noreply.github.com>
2023-11-03 10:20:22 -0400

    comments 2

--- Main/src/Game.java
+++ Main/src/Game.java
@@ -22,7 +22,7 @@ public class Game {
     String[] cogNames = {"Flunky","Bottom Feeder","Short Change","Cold Caller","Pencil Pusher","Bloodsucker","Penny Pincher","Telemarketer","Yesman","Double Talker","Tightwad","Name Dropper","Micromanager","Ambulance Chaser","Bean Counter","Glad Hander","Downsizer","Back Stabber","Number Cruncher","Mover & Shaker","Head Hunter","Spin Doctor","Money Bags","Two-Face","Corporate Raider","Legal Eagle","Loan Shark","Mingler","The Big Cheese","Big Wig","Robber Baron","Mr. Hollywood"};
     // length = 32, 0-31
     // 1: 0-3 2: 0-7 3: 0-11 4: 0-15 5: 0-19 6: 4-23 7: 8-27 9: 12-31 10: 20-31 11: 24-31 12: 27-31 (edit lator)
-    String chosenCogName = cogNames[rand.nextInt]
+    String chosenCogName = cogNames[rand.nextInt] //incorporate algorithm
     
     Cog c = new Cog(cogLevel); // we'll think about it
     Toon p = new Toon((int)((c.getLevel() + 0.3) * (c.getLevel() + 0.2)));
