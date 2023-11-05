8876b187e0a1279dfd9618c0b35996fbab8217ba
grammarandsyntax <144465898+grammarandsyntax@users.noreply.github.com>
2023-11-05 13:49:56 -0500

    Working cog generator
    Thanks to Asher for clamp idea + index equations + bug fixing

--- Main/src/Cog.java
+++ Main/src/Cog.java
@@ -3,26 +3,43 @@ public class Cog {
     private String name;
     private int level;
     private int hp;
+    private String[] cogNames = {"Flunky","Bottom Feeder","Short Change","Cold Caller","Pencil Pusher","Bloodsucker","Penny Pincher","Telemarketer","Yesman","Double Talker","Tightwad","Name Dropper","Micromanager","Ambulance Chaser","Bean Counter","Glad Hander","Downsizer","Back Stabber","Number Cruncher","Mover & Shaker","Head Hunter","Spin Doctor","Money Bags","Two-Face","Corporate Raider","Legal Eagle","Loan Shark","Mingler","The Big Cheese","Big Wig","Robber Baron","Mr. Hollywood"};
+    final private int minIndex;
+    final private int maxIndex;
+
+    static int clamp(int x, int min, int max){
+      return Math.max(min, Math.min(max, x));
+    }
     
-
-    public Cog (String n, int l){
-        name = n;
-        level = l;
-        hp = (level+1)*(level+2);
-    }
-    public Cog (int l){
-      level = l;
+    public Cog (){
+      level = (int)(Math.random()*(11)+1)+1;
       hp = (level+1)*(level+2);
+      //THANKS ASHER
+      minIndex = clamp(((level*4)-20),0,31);
+      maxIndex = clamp(((level*4)-1),0,31);
+      name = cogNames[(int)(Math.random()*(maxIndex-minIndex+1))+minIndex];
     }
 
-    //get
+    //get + set
+    public int getminindex(){
+      return minIndex;
+    }
+    public int getmaxindex(){
+      return maxIndex;
+    }
     public String getName(){
       return name;
     }
+    public void setName(String name){
+      this.name=name;
+    }
     public int getLevel(){
-        return level;
+      return level;
+    }
+    public void setLevel(int level){
+      this.level=level;
     }
     public int getHp(){
-        return hp;
+      return hp;
     }
 }
