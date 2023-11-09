36debef839321ac76f8130c9a1b89afe013e1789
grammarandsyntax <yevak2@nycstudents.net>
2023-11-09 10:09:50 -0500

    Added class for menu

--- Main/src/Game.java
+++ Main/src/Game.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 public class Game {
   static void Wait(int milliseconds){
     try {
@@ -8,23 +7,19 @@ public class Game {
       Thread.currentThread().interrupt();
     }
   }
-  
   //runner
   public Game(){
     setup();
   }
   
   public void setup() {
-
     Cog c = new Cog();
-    
     //for testing, remove when done
     System.out.println(c.getLevel());
     System.out.println(c.getName());
     System.out.println(c.getHp());
-  
+    //toon testing
     Toon p = new Toon((int)((c.getLevel() + 0.3) * (c.getLevel() + 0.2))); //calculations work, level calculation subject to change
     System.out.println(p.getLaff());
-    
   }
 }
