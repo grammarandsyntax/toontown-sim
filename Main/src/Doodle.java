9969d58b8e6a76949c6720f50c8273a4c7319895
BT_2N2_11 <yevak2@nycstudents.net>
2023-12-06 10:03:53 -0500

    Doodle beginning

--- Main/src/Doodle.java
+++ Main/src/Doodle.java
@@ -1,7 +1,9 @@
 public class Doodle {
     //traits in order of largest absolute multiplier to smallest
-    private String[] negativeTraits = {"forgets","grumpy","restless"};
-    private String[] positiveTraits = {"affectionate","playful","calm"};
+    private final String[] negativeTraitsAll = {"forgets","grumpy","restless"};
+    private final String[] positiveTraitsAll = {"affectionate","playful","calm"};
+    private String negativeTrait;
+    private String positiveTrait;
 
     private int jumpLevel = 0;
     private int rolloverLevel = 0;
